fcc23e0d8fe1de1f3b647c09e95afda785bd6b44
assegaf1010 <muhammadassegaf53@gmail.com>
2025-01-14 14:26:06 +0700

    vercel1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
-/** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme')
-
-export default {
-    content: [
-      "./Resources/**/*.blade.php",
-      "./resources/**/*.js",
-      "./resources/**/*.vue",
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-            sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
-          },
-      },
-    },
-    plugins: [],
-  }
+module.exports = {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
