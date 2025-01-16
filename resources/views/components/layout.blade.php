9c037f813cfb3d3135e20253b6c5eb9129839c5c
assegaf1010 <muhammadassegaf53@gmail.com>
2025-01-16 15:23:18 +0700

    important

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WSF</title>
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
-    @vite('resources/css/app.css')
+    <script src="https://cdn.tailwindcss.com"></script>
 
 </head>
 <body class="">
